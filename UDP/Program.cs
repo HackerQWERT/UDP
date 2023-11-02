fedf9079326feb68c788e0292e4938dcf7b49edb
HackerQWERT <HackerQWERT@outlook.com>
2023-11-03 01:40:25 +0800

    Update

--- UDP/Program.cs
+++ UDP/Program.cs
@@ -3,7 +3,8 @@
 
 
 MyUdpClient myUdpClient = new MyUdpClient("127.0.0.1", 1111);
-myUdpClient.OnReceiveMessage += async (sender, e) =>
+
+myUdpClient.OnReceiveMessageEvent += async (sender, e) =>
 {
     Console.WriteLine($"接收到消息：{e.Message}");
     await myUdpClient.SendAsync("我收到了你的消息");
